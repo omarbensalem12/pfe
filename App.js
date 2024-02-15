308c4c949bf27ecf1060729b61f43c2e9e2fd19c
omar321 <obensalem7@gmail.com>
2024-02-15 11:41:53 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import StackNavigator from "./StackNavigator";
+import { UserContext } from "./UserContext";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { MyProvider } from "./MyContext";
+import i18n from './i18/i18n';
+const queryClient = new QueryClient();
+export default function App() {
+  i18n.init();
+  return (
+    <MyProvider>
+      <QueryClientProvider client={queryClient}>
+        <UserContext>
+          <StackNavigator />
+        </UserContext>
+      </QueryClientProvider>
+    </MyProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
